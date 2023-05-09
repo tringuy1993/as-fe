4574b17733d58c6b234cb454cb63a7f5a53aaf8c
Tri Nguyen <triguy1993@gmail.com>
2023-05-08 22:14:33 -0700

    Update EChartTime work!

--- components/ECharts/Time/EChartTime.js
+++ components/ECharts/Time/EChartTime.js
@@ -1,41 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-import { ECOpts_Time, EChartTime_Opts } from "./EChartTime_Opts";
+import { EChartTime_Opts } from "./EChartTime_Opts";
 import ReactEcharts from "echarts-for-react";
-import { GetAllModifiedToSData, GetModifiedToSData, combineESOptionData } from "../DataEChart";
-import { modify_data } from "../UtilECharts";
-
-const EChartTime = ({ symbol, data, greek, theme }) => {
-  const [chartOptions, setChartOptions] = useState({});
-  const [chartHeight, setChartHeight] = useState({});
 
 
-  function getData (){
-    setChartHeight([]);
-    setChartOptions([]);
-    let option, chartHeight
-    if (data){
-      ({option, chartHeight}= EChartTime_Opts(data));
-      setChartOptions(option)
-      setChartHeight(chartHeight);
-    }
-    // ({option, chartHeight}= EChartTime_Opts(data));
+const EChartTime = ({data, theme }) => {
+  let option, chartHeight, chartOptions
+  if(data){
+    option = EChartTime_Opts(data);
+    chartOptions = option.option
+    chartHeight = option.chartHeight
 
   }
-
-  useEffect(() => {
-    getData()
-  }, [data, greek, symbol]);
-
   return (
     <>
-      {Object.keys(chartOptions).length && (
         <ReactEcharts
           option={{ ...chartOptions }}
           style={{ height: chartHeight }}
           theme={theme}
+          notMerge={true}
         ></ReactEcharts>
-      )}
     </>
   );
 };
